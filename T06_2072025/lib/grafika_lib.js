990ca2bef86e1b798cd219002cba42a7019e0288
sherly santiadi <sntdshrly@gmail.com>
2022-10-25 20:20:56 +0700

    push

--- T06_2072025/lib/grafika_lib.js
+++ T06_2072025/lib/grafika_lib.js
@@ -97,22 +97,22 @@ class MatrixGrafkom {
 
 }
 
-function polygon(points, color) {
-  ctx.strokeStyle = color;
+// function polygon(points, color) {
+//   ctx.strokeStyle = color;
 
-  let [x0, y0] = points[0];
-  ctx.moveTo(x0, y0);
-  ctx.beginPath();
-  points.forEach((p) => {
-    let [x, y] = p;
-    ctx.lineTo(x, y);
-    ctx.moveTo(x, y);
-  });
-  ctx.lineTo(x0, y0);
-  ctx.stroke();
+//   let [x0, y0] = points[0];
+//   ctx.moveTo(x0, y0);
+//   ctx.beginPath();
+//   points.forEach((p) => {
+//     let [x, y] = p;
+//     ctx.lineTo(x, y);
+//     ctx.moveTo(x, y);
+//   });
+//   ctx.lineTo(x0, y0);
+//   ctx.stroke();
 
-  ctx.strokeStyle = "black";
-}
+//   ctx.strokeStyle = "black";
+// }
 function gbr_titik(imageDataTemp, x, y, r, g, b) {
   // if ((x>0)&&(x<canvasKita.width) && (y>0) && (y<canvasKita.height)){
   // garis x sama y dibulatkan
@@ -136,23 +136,132 @@ function floodFillStack(imageDataSaya, canvas, x0, y0, toFlood, color) {
   var tumpukan = [];
   tumpukan.push({ x: x0, y: y0 });
   while (tumpukan.length > 0) {
-      // ambil 1 titik, trs cek bisa diwarna atau ga, kalau bisa warna lalu masukan dalam tumpukan titik sekitarnya
-      var titik_skrg = tumpukan.pop();
-      var index_skrg = 4 * (titik_skrg.x + (titik_skrg.y * canvas.width));
-      var r1 = imageDataSaya.data[index_skrg];
-      var g1 = imageDataSaya.data[index_skrg + 1];
-      var b1 = imageDataSaya.data[index_skrg + 2];
-      if (r1 == toFlood.r && g1 == toFlood.g && b1 == toFlood.b) {
-          // mau ganti warna
-          imageDataSaya.data[index_skrg] = color.r;
-          imageDataSaya.data[index_skrg + 1] = color.g;
-          imageDataSaya.data[index_skrg + 2] = color.b;
-          imageDataSaya.data[index_skrg + 3] = 255;
+    // ambil 1 titik, trs cek bisa diwarna atau ga, kalau bisa warna lalu masukan dalam tumpukan titik sekitarnya
+    var titik_skrg = tumpukan.pop();
+    var index_skrg = 4 * (titik_skrg.x + (titik_skrg.y * canvas.width));
+    var r1 = imageDataSaya.data[index_skrg];
+    var g1 = imageDataSaya.data[index_skrg + 1];
+    var b1 = imageDataSaya.data[index_skrg + 2];
+    if (r1 == toFlood.r && g1 == toFlood.g && b1 == toFlood.b) {
+      // mau ganti warna
+      imageDataSaya.data[index_skrg] = color.r;
+      imageDataSaya.data[index_skrg + 1] = color.g;
+      imageDataSaya.data[index_skrg + 2] = color.b;
+      imageDataSaya.data[index_skrg + 3] = 255;
 
-          tumpukan.push({ x: titik_skrg.x + 1, y: titik_skrg.y })
-          tumpukan.push({ x: titik_skrg.x - 1, y: titik_skrg.y })
-          tumpukan.push({ x: titik_skrg.x, y: titik_skrg.y + 1 })
-          tumpukan.push({ x: titik_skrg.x, y: titik_skrg.y - 1 })
+      tumpukan.push({ x: titik_skrg.x + 1, y: titik_skrg.y })
+      tumpukan.push({ x: titik_skrg.x - 1, y: titik_skrg.y })
+      tumpukan.push({ x: titik_skrg.x, y: titik_skrg.y + 1 })
+      tumpukan.push({ x: titik_skrg.x, y: titik_skrg.y - 1 })
+    }
+  }
+  ctx.putImageData(imageDataSaya, 0, 0);
+}
+function gbr_garis(imageDataSaya, x1, y1, x2, y2, r, g, b) {
+  var dx = x2 - x1;
+  var dy = y2 - y1;
+  // gambar garis jalan di x
+  if (Math.abs(dx) > Math.abs(dy)) {
+      // ke kanan   
+      if (x2 > x1) {
+          var y = y1;
+          for (var x = x1; x < x2; x++) {
+              y = y + dy / Math.abs(dx); //1/m
+              gbr_titik(imageDataSaya, x, y, r, g, b)
+          }
+      }
+      else {
+          // ke kiri
+          var y = y1;
+          for (var x = x1; x > x2; x--) {
+              y = y + dy / Math.abs(dx); //1/m
+              gbr_titik(imageDataSaya, x, y, r, g, b)
+          }
+      }
+  }
+  // jalan di y
+  else {
+      if (y2 > y1) {
+          // ke atas   
+          var x = x1;
+          for (var y = y1; y < y2; y++) {
+              x = x + dx / Math.abs(dy); //m
+              gbr_titik(imageDataSaya, x, y, r, g, b)
+          }
+      }
+      else {
+          // ke bawah
+          var x = x1;
+          for (var y = y1; y > y2; y--) {
+              x = x + dx / Math.abs(dy); //m
+              gbr_titik(imageDataSaya, x, y, r, g, b)
+          }
       }
   }
 }
+function polygon_matriks(imageDataSaya, titik, warna) {
+r = warna[0];
+g = warna[1];
+b = warna[2];
+  for (i = 0; i < titik.length - 1; i++) {
+      x1 = titik[i][0];
+      y1 = titik[i][1];
+      x2 = titik[i + 1][0];
+      y2 = titik[i + 1][1];
+      gbr_garis(imageDataSaya, x1, y1, x2, y2, r, g, b);
+  }
+  gbr_garis(imageDataSaya, titik[titik.length - 1][0], titik[titik.length - 1][1], titik[0][0], titik[0][1], r, g, b);
+}
+function gbr_titik_arr(datanya, posisi, warna) {
+  let { x, y } = posisi;
+  let { r, g, b } = warna;
+  let index = 4 * (Math.ceil(x) + (Math.ceil(y) * canvasKita.width));
+  datanya.data[index] = r;
+  datanya.data[index + 1] = g;
+  datanya.data[index + 2] = b
+  datanya.data[index + 3] = 255;
+}
+function get_warna(datanya, posisi) {
+  let { x, y } = posisi;
+  let index = (x + y * canvasKita.width) * 4;
+  let r, g, b;
+  r = datanya.data[index];
+  g = datanya.data[index + 1];
+  b = datanya.data[index + 2];
+  return { r: r, g: g, b: b };
+};
+function cekWarna(color1, color2) {
+  if (color1.r == color2.r && color1.g == color2.g && color1.b == color2.b) {
+      return true
+  }
+  else {
+      return false
+  }
+}
+function boundaryFillStack(datanya, posisi, batas, warna) {
+  // kalau titik yg di posisi bisa diwarna, maka warnai dulu
+  let warna_titik_skrg = get_warna(datanya, posisi);
+  let my_stack = [];
+  let { x, y } = posisi;
+  my_stack.push(posisi);
+  while (my_stack.length > 0) {
+      posisi = my_stack.pop();
+      warna_titik_skrg = get_warna(datanya, posisi);
+      if (!cekWarna(warna_titik_skrg, batas)) {
+          gbr_titik_arr(datanya, posisi, warna);
+          my_stack.push({ x: posisi.x + 1, y: posisi.y });
+          my_stack.push({ x: posisi.x, y: posisi.y + 1 });
+          my_stack.push({ x: posisi.x - 1, y: posisi.y });
+          my_stack.push({ x: posisi.x, y: posisi.y - 1 });
+      }
+  }
+}
+// function gbr_titik(datanya,posisi,warna){
+//   let {x,y} = posisi;
+//   let {r,g,b} = warna;
+//   let index = (x+y*canvasKita.width)*4;
+//   datanya.data[index] = r;
+//   datanya.data[index+1] = g;
+//   datanya.data[index+2] = b
+//   datanya.data[index+3] = 255;
+// }
